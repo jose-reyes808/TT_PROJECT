cdd74b464a1cc8ffe81c79f9b77eaffdd8403489
joze1104 <89559626+joze1104@users.noreply.github.com>
2021-11-26 17:32:24 -0800

    finished code just need to add logic for when to turn on the motor

--- project.ino
+++ project.ino
@@ -1,59 +1,76 @@
 #include <LiquidCrystal.h>
-
+// Pin constants
 const int tempSensorPin = A0;
 const int lightSensorPin = A1;
 const int moistureSensorPin = A2;
 
+// Moisture Sensor constants
+const int AirValue = 444;   // value when dry
+const int WaterValue = 208; // value when wet
+
+// Variables
 float tempSensorValue;
 float voltageOut;
 int lightValue;
-float moistureSensorValue;
-
 float temperatureF;
+int soilMoistureValue = 0;
+int soilmoisturepercent=0;
 
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
+// Setup LCD pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
+  // Set pins as input
   pinMode(tempSensorPin, INPUT);
-  Serial.begin(9600);     
+  pinMode(lightSensorPin, INPUT);
+  pinMode(moistureSensorPin, INPUT);
+  
+  // Begin LCD function
+  lcd.begin(16, 2);
 }
+
 void loop() {
-  // Sensor Value
-  tempSensorValue = analogRead(tempSensorPin);    //Read the analog pin
+  // Read Sensor Value
+  tempSensorValue = analogRead(tempSensorPin);    //Read the analog 
+  // Convert to Farenheit
   voltageOut = (tempSensorValue * 5000) / 1024;
   temperatureF = voltageOut / 10;
 
-  // Photoresistor Value
+  // Read Photoresistor Value
   lightValue = analogRead(lightSensorPin);
 
-  // Moisture Sensor Value
-   for (int i = 0; i <= 100; i++) 
-   { 
-     moistureSensorValue = moistureSensorValue + analogRead(moistureSensorPin); 
-     delay(1); 
-   } 
-   moistureSensorValue = moistureSensorValue/100.0; 
-   Serial.print("Moisture: ");
-   Serial.println(moistureSensorValue); 
+  // Read Moisture Value
+  soilMoistureValue = analogRead(A2);
+  soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
 
-  // Print Data
-  Serial.print("Temperature: ");
-  Serial.print(temperatureF);
-  Serial.println("F");  //print the temperature status
-
-  Serial.print("Light Value: ");
-  Serial.println(lightValue);
-
-  lcd.begin(16, 2);
-  lcd.clear();
-
+  // Print moisture sensor values to row 0
   lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
+  lcd.print("Moisture:");
+  if(soilmoisturepercent >= 100)
+  {
+    lcd.print("100 %");
+  }
+  else if(soilmoisturepercent <=0)
+  {
+    lcd.print("0 %");
+  }
+  else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
+  {
+    lcd.print(soilmoisturepercent);
+    lcd.print("%");
+    lcd.print("  ");
+    
+  }
+  
+  // Print Temp and Light values to LCD row 1
+  lcd.setCursor(0, 1);
+  lcd.print("T:");
   lcd.print(temperatureF);
   lcd.print("F");
+  lcd.print(" L:");
+  lcd.print(lightValue);
+  lcd.print(" ");
   
-  
+  // Update every 1000 ms
   delay(1000);
-    
 }
